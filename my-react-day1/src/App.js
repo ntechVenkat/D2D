44d4466d8196d6344d7f7747c215720b3fbff530
sunilFrtnd <tvsunil4@gmail.com>
2025-02-20 18:55:06 +0530

    Code Before Commit-Code in progress

---
+++ my-react-day1/src/App.js
@@ -0,0 +1,10 @@
+const library = "React";
+const App = () => {
+  <div>
+    <center>
+      <h1>Hello World!</h1>
+      <p>This will tells about {library}</p>
+    </center>
+  </div>;
+};
+export default App;
